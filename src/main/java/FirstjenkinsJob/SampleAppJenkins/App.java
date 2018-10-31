f71284f22fe8e290bd8d31328e8d78bfa02e4aca
SaiSharvani <popuri.sai.sharvani@accenture.com>
2018-10-31 10:25:09 +0530

    Commit on oct31

--- src/main/java/FirstjenkinsJob/SampleAppJenkins/App.java
+++ src/main/java/FirstjenkinsJob/SampleAppJenkins/App.java
@@ -9,5 +9,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        	 for(int i=1;i<=10;i++) {
+        	        	for(int j=1;j<=i;j++) {
+        	        	
+        	        	System.out.print(j);
+        	        	}
+        	        	System.out.println();
+        	        } 
+        	 
+
     }
 }
